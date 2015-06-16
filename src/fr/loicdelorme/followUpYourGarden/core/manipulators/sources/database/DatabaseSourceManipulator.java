d1f720bf3588918bd92e745e535a887dfd42258f
DELORME Loïc <loic.delorme.pro@gmail.com>
2015-06-16 08:56:22 +0200

    [add] implementation to handle database data sources.

---
+++ src/fr/loicdelorme/followUpYourGarden/core/manipulators/sources/database/DatabaseSourceManipulator.java
@@ -0,0 +1,99 @@
+package fr.loicdelorme.followUpYourGarden.core.manipulators.sources.database;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+import fr.loicdelorme.followUpYourGarden.core.manipulators.sources.ISourceManipulator;
+
+/**
+ * This class allow you to handle a connection with a database.
+ * 
+ * @author DELORME Loïc
+ * @version 1.0.0
+ */
+public class DatabaseSourceManipulator implements ISourceManipulator
+{
+	/**
+	 * The database URL.
+	 */
+	private final String url;
+
+	/**
+	 * The username.
+	 */
+	private final String username;
+
+	/**
+	 * The password.
+	 */
+	private final String password;
+
+	/**
+	 * The connection.
+	 */
+	private Connection connection;
+
+	/**
+	 * If you are connected.
+	 */
+	private boolean isConnected;
+
+	/**
+	 * Create a database data source manipulator.
+	 * 
+	 * @param driver
+	 *            The driver name.
+	 * @param url
+	 *            The database URL.
+	 * @param username
+	 *            The username.
+	 * @param password
+	 *            The password.
+	 * @throws ClassNotFoundException
+	 *             If the driver is not found.
+	 */
+	public DatabaseSourceManipulator(String driver, String url, String username, String password) throws ClassNotFoundException
+	{
+		Class.forName(driver);
+
+		this.url = url;
+		this.username = username;
+		this.password = password;
+		this.connection = null;
+		this.isConnected = false;
+	}
+
+	@Override
+	public void openConnection() throws SQLException
+	{
+		if (!this.isConnected)
+		{
+			this.connection = DriverManager.getConnection(this.url, this.username, this.password);
+			this.isConnected = true;
+		}
+	}
+
+	@Override
+	public boolean isConnected()
+	{
+		return this.isConnected;
+	}
+
+	@Override
+	public Connection getConnection()
+	{
+		return this.connection;
+	}
+
+	@Override
+	public void closeConnection() throws SQLException
+	{
+		if (this.isConnected)
+		{
+			this.connection.close();
+			this.connection = null;
+			this.isConnected = false;
+		}
+	}
+}
