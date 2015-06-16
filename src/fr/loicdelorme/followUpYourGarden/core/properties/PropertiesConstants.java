eb1079696a2834b1138218ec0a36a87db66b43b1
DELORME Loïc <loic.delorme.pro@gmail.com>
2015-06-16 08:57:05 +0200

    [add] interface to resume all files paths.

---
+++ src/fr/loicdelorme/followUpYourGarden/core/properties/PropertiesConstants.java
@@ -0,0 +1,15 @@
+package fr.loicdelorme.followUpYourGarden.core.properties;
+
+/**
+ * This interface allow you to handle properties constants.
+ * 
+ * @author DELORME Loïc
+ * @version 1.0.0
+ */
+public interface PropertiesConstants
+{
+	/**
+	 * The database configuration file.
+	 */
+	public static final String DATABASE_CONFIGURATION_FILE = "data/configuration/database.properties";
+}
