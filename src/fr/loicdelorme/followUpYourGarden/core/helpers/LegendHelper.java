4504fdd5aa0feb49a34ca35b81e63e76ee5f4290
DELORME Loïc <loic.delorme.pro@gmail.com>
2015-06-21 10:59:57 +0200

    [add] helper.

---
+++ src/fr/loicdelorme/followUpYourGarden/core/helpers/LegendHelper.java
@@ -0,0 +1,36 @@
+package fr.loicdelorme.followUpYourGarden.core.helpers;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import fr.loicdelorme.followUpYourGarden.core.models.GroupOfPlants;
+import fr.loicdelorme.followUpYourGarden.core.models.LegendItem;
+
+/**
+ * This class allow you to handle legend items.
+ * 
+ * @author DELORME Loïc
+ * @version 1.0.0
+ */
+public class LegendHelper
+{
+	/**
+	 * Generate the legend.
+	 * 
+	 * @param groupsOfPlants
+	 *            The list of groups of plants.
+	 * 
+	 * @return A list of legend items.
+	 */
+	public List<LegendItem> generateLegend(List<GroupOfPlants> groupsOfPlants)
+	{
+		List<LegendItem> legendItems = new ArrayList<LegendItem>();
+
+		for (GroupOfPlants currentGroupOfPlants : groupsOfPlants)
+		{
+			legendItems.add(new LegendItem(currentGroupOfPlants.getWording(), currentGroupOfPlants.getIconColor()));
+		}
+
+		return legendItems;
+	}
+}
