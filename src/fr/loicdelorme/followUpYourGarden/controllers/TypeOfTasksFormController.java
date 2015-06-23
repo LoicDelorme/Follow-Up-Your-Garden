902e9d570157763008a9b56ae86cd263073eff19
DELORME Loïc <loic.delorme.pro@gmail.com>
2015-06-23 17:36:34 +0200

    [update] controller.

--- src/fr/loicdelorme/followUpYourGarden/controllers/TypeOfTasksFormController.java
+++ src/fr/loicdelorme/followUpYourGarden/controllers/TypeOfTasksFormController.java
@@ -135,13 +135,13 @@ public class TypeOfTasksFormController extends Controller
 		{
 			if (this.isUpdateForm)
 			{
-				this.typeOfTasksServices.updateTypeOfTasks(this.wording.getText(), this.description.getText(), this.typeOfTasks);
+				this.typeOfTasksServices.updateTypeOfTasks(this.wording.getText().trim(), this.description.getText().trim(), this.typeOfTasks);
 				this.displayInformation(this.bundle.getString("operationSuccess"), null, this.bundle.getString("typeOfTasksModificationSuccess"));
 				this.stage.close();
 			}
 			else
 			{
-				this.typeOfTasksServices.addTypeOfTasks(this.wording.getText(), this.description.getText());
+				this.typeOfTasksServices.addTypeOfTasks(this.wording.getText().trim(), this.description.getText().trim());
 				this.displayInformation(this.bundle.getString("operationSuccess"), null, this.bundle.getString("typeOfTasksAdditionSuccess"));
 				this.stage.close();
 			}
