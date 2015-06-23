103a10598b16331ff9a5dd71c4a76358e6f03a71
DELORME Loïc <loic.delorme.pro@gmail.com>
2015-06-23 09:11:05 +0200

    [add] interface.

---
+++ src/fr/loicdelorme/followUpYourGarden/core/properties/ILanguageReader.java
@@ -0,0 +1,17 @@
+package fr.loicdelorme.followUpYourGarden.core.properties;
+
+/**
+ * This interface allow you to read language data.
+ * 
+ * @author DELORME Loïc
+ * @version 1.0.0
+ */
+public interface ILanguageReader
+{
+	/**
+	 * Get the language.
+	 * 
+	 * @return The language.
+	 */
+	public String getLanguage();
+}
