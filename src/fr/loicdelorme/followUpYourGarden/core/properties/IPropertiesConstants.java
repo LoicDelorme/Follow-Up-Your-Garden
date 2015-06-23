025ad9605249290cbc41d0eb0a1c1250586abb77
DELORME Loïc <loic.delorme.pro@gmail.com>
2015-06-23 09:10:37 +0200

    [update] constants.

--- src/fr/loicdelorme/followUpYourGarden/core/properties/IPropertiesConstants.java
+++ src/fr/loicdelorme/followUpYourGarden/core/properties/IPropertiesConstants.java
@@ -12,9 +12,14 @@ public interface IPropertiesConstants
 	 * The database configuration file.
 	 */
 	public static final String DATABASE_CONFIGURATION_FILE = "data/configuration/database.properties";
-	
+
 	/**
 	 * The dimensions configuration file.
 	 */
 	public static final String DIMENSIONS_CONFIGURATION_FILE = "data/configuration/dimensions.properties";
+
+	/**
+	 * The language configuration file.
+	 */
+	public static final String LANGUAGE_CONFIGURATION_FILE = "data/configuration/language.properties";
 }
