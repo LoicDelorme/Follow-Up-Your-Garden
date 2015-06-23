9d948d69a30051caa25fd77d2dda10b764a815cf
DELORME Loïc <loic.delorme.pro@gmail.com>
2015-06-23 14:58:27 +0200

    [add] enumeration.

---
+++ src/fr/loicdelorme/followUpYourGarden/core/models/ContentSelectorType.java
@@ -0,0 +1,30 @@
+package fr.loicdelorme.followUpYourGarden.core.models;
+
+/**
+ * This enumeration allow you to handle several actions.
+ * 
+ * @author DELORME Loïc
+ * @version 1.0.0
+ */
+public enum ContentSelectorType
+{
+	/**
+	 * Update a type of plants.
+	 */
+	UPDATE_TYPE_OF_PLANTS,
+
+	/**
+	 * Update a type of tasks.
+	 */
+	UPDATE_TYPE_OF_TASKS,
+
+	/**
+	 * Remove a type of plants.
+	 */
+	REMOVE_TYPE_OF_PLANTS,
+
+	/**
+	 * Remove a type of tasks.
+	 */
+	REMOVE_TYPE_OF_TASKS;
+}
