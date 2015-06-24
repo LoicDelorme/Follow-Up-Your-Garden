687aa42a4de8be8357dddff0f99fec9a4feb22b4
DELORME Loïc <loic.delorme.pro@gmail.com>
2015-06-24 11:50:26 +0200

    [update] exception.

--- src/fr/loicdelorme/followUpYourGarden/core/services/exceptions/MissingCarriedOutTaskTypeOfTasksException.java
+++ src/fr/loicdelorme/followUpYourGarden/core/services/exceptions/MissingCarriedOutTaskTypeOfTasksException.java
@@ -1,5 +1,7 @@
 package fr.loicdelorme.followUpYourGarden.core.services.exceptions;
 
+import fr.loicdelorme.followUpYourGarden.core.language.MyResourceBundle;
+
 /**
  * This exception is thrown if the typeOfTasks attribute is missing.
  * 
@@ -14,6 +16,6 @@ public class MissingCarriedOutTaskTypeOfTasksException extends Exception
 	 */
 	public MissingCarriedOutTaskTypeOfTasksException()
 	{
-		super("The type of tasks is missing!");
+		super(MyResourceBundle.getBundle().getString("missingCarriedOutTaskTypeOfTasksException"));
 	}
 }
