cfb07529373f57e5c51ae497a87148c662a28edc
DELORME Loïc <loic.delorme.pro@gmail.com>
2015-06-24 10:44:31 +0200

    [add] helper.

---
+++ src/fr/loicdelorme/followUpYourGarden/core/helpers/TextComputerHelper.java
@@ -0,0 +1,36 @@
+package fr.loicdelorme.followUpYourGarden.core.helpers;
+
+import java.util.List;
+
+import fr.loicdelorme.followUpYourGarden.core.models.CarriedOutTask;
+
+/**
+ * This class allow you to handle texts.
+ * 
+ * @author DELORME Loïc
+ * @version 1.0.0
+ */
+public class TextComputerHelper
+{
+	/**
+	 * Compute carried out tasks text.
+	 * 
+	 * @param carriedOutTasks
+	 *            The list of carried out tasks.
+	 * @return The computed text.
+	 */
+	public static String computeCarriedOutTasksText(List<CarriedOutTask> carriedOutTasks)
+	{
+		StringBuilder result = new StringBuilder();
+
+		for (CarriedOutTask currentCarriedOutTask : carriedOutTasks)
+		{
+			result.append(currentCarriedOutTask.getTypeOfTasks().getWording());
+			result.append(" : ");
+			result.append(currentCarriedOutTask.getCarriedOutDate());
+			result.append("\n");
+		}
+
+		return result.toString();
+	}
+}
