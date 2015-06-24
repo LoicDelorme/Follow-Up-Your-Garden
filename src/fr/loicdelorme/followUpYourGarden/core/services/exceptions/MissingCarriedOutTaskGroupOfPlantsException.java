f33578f6f6c6c5b5563e6440303171f36cf8ef49
DELORME Loïc <loic.delorme.pro@gmail.com>
2015-06-24 11:50:29 +0200

    [update] exception.

--- src/fr/loicdelorme/followUpYourGarden/core/services/exceptions/MissingCarriedOutTaskGroupOfPlantsException.java
+++ src/fr/loicdelorme/followUpYourGarden/core/services/exceptions/MissingCarriedOutTaskGroupOfPlantsException.java
@@ -1,5 +1,7 @@
 package fr.loicdelorme.followUpYourGarden.core.services.exceptions;
 
+import fr.loicdelorme.followUpYourGarden.core.language.MyResourceBundle;
+
 /**
  * This exception is thrown if the groupOfPlants attribute is missing.
  * 
@@ -14,6 +16,6 @@ public class MissingCarriedOutTaskGroupOfPlantsException extends Exception
 	 */
 	public MissingCarriedOutTaskGroupOfPlantsException()
 	{
-		super("The group of plants is missing!");
+		super(MyResourceBundle.getBundle().getString("missingCarriedOutTaskGroupOfPlantsException"));
 	}
 }
