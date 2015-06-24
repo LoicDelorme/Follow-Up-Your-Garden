054e9d741e2a950e6af80f6bc73973e3e2a12faf
DELORME Loïc <loic.delorme.pro@gmail.com>
2015-06-24 11:50:52 +0200

    [update] exception.

--- src/fr/loicdelorme/followUpYourGarden/core/services/exceptions/MissingTaskToBeCarryOutCurrentProgressionException.java
+++ src/fr/loicdelorme/followUpYourGarden/core/services/exceptions/MissingTaskToBeCarryOutCurrentProgressionException.java
@@ -1,5 +1,7 @@
 package fr.loicdelorme.followUpYourGarden.core.services.exceptions;
 
+import fr.loicdelorme.followUpYourGarden.core.language.MyResourceBundle;
+
 /**
  * This exception is thrown if the currentProgression attribute is missing.
  * 
@@ -14,6 +16,6 @@ public class MissingTaskToBeCarryOutCurrentProgressionException extends Exceptio
 	 */
 	public MissingTaskToBeCarryOutCurrentProgressionException()
 	{
-		super("The current progression is missing!");
+		super(MyResourceBundle.getBundle().getString("missingTaskToBeCarryOutCurrentProgressionException"));
 	}
 }
