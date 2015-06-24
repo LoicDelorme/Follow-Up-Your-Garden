ec611cba05cbdbef88671d4c8c711bd15b4a90f4
DELORME Loïc <loic.delorme.pro@gmail.com>
2015-06-24 11:50:38 +0200

    [update] exception.

--- src/fr/loicdelorme/followUpYourGarden/core/services/exceptions/MissingGroupOfPlantsIconColorException.java
+++ src/fr/loicdelorme/followUpYourGarden/core/services/exceptions/MissingGroupOfPlantsIconColorException.java
@@ -1,5 +1,7 @@
 package fr.loicdelorme.followUpYourGarden.core.services.exceptions;
 
+import fr.loicdelorme.followUpYourGarden.core.language.MyResourceBundle;
+
 /**
  * This exception is thrown if the iconColor attribute is missing.
  * 
@@ -14,6 +16,6 @@ public class MissingGroupOfPlantsIconColorException extends Exception
 	 */
 	public MissingGroupOfPlantsIconColorException()
 	{
-		super("The icon color is missing!");
+		super(MyResourceBundle.getBundle().getString("missingGroupOfPlantsIconColorException"));
 	}
 }
