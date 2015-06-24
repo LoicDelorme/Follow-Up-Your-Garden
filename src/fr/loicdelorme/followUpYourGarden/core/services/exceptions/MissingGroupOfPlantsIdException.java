8884859ba93d893c12a5f3e8ac40293d1a4f7e53
DELORME Loïc <loic.delorme.pro@gmail.com>
2015-06-24 11:50:31 +0200

    [update] exception.

--- src/fr/loicdelorme/followUpYourGarden/core/services/exceptions/MissingGroupOfPlantsIdException.java
+++ src/fr/loicdelorme/followUpYourGarden/core/services/exceptions/MissingGroupOfPlantsIdException.java
@@ -1,5 +1,7 @@
 package fr.loicdelorme.followUpYourGarden.core.services.exceptions;
 
+import fr.loicdelorme.followUpYourGarden.core.language.MyResourceBundle;
+
 /**
  * This exception is thrown if the id attribute is missing.
  * 
@@ -14,6 +16,6 @@ public class MissingGroupOfPlantsIdException extends Exception
 	 */
 	public MissingGroupOfPlantsIdException()
 	{
-		super("The id is missing!");
+		super(MyResourceBundle.getBundle().getString("missingGroupOfPlantsIdException"));
 	}
 }
