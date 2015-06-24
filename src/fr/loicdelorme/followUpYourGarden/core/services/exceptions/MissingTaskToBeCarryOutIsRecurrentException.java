c7e82aba85d027245705abfd18945d9c121d6923
DELORME Loïc <loic.delorme.pro@gmail.com>
2015-06-24 11:51:01 +0200

    [update] exception.

--- src/fr/loicdelorme/followUpYourGarden/core/services/exceptions/MissingTaskToBeCarryOutIsRecurrentException.java
+++ src/fr/loicdelorme/followUpYourGarden/core/services/exceptions/MissingTaskToBeCarryOutIsRecurrentException.java
@@ -1,5 +1,7 @@
 package fr.loicdelorme.followUpYourGarden.core.services.exceptions;
 
+import fr.loicdelorme.followUpYourGarden.core.language.MyResourceBundle;
+
 /**
  * This exception is thrown if the isRecurrent attribute is missing.
  * 
@@ -14,6 +16,6 @@ public class MissingTaskToBeCarryOutIsRecurrentException extends Exception
 	 */
 	public MissingTaskToBeCarryOutIsRecurrentException()
 	{
-		super("The recurrent value is missing!");
+		super(MyResourceBundle.getBundle().getString("missingTaskToBeCarryOutIsRecurrentException"));
 	}
 }
