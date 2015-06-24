34ee5f6670d64cdf5978bfc474f9a3d19a2b4e42
DELORME Loïc <loic.delorme.pro@gmail.com>
2015-06-24 11:50:21 +0200

    [update] exception.

--- src/fr/loicdelorme/followUpYourGarden/core/services/exceptions/InvalidGroupOfPlantsWordingException.java
+++ src/fr/loicdelorme/followUpYourGarden/core/services/exceptions/InvalidGroupOfPlantsWordingException.java
@@ -1,5 +1,7 @@
 package fr.loicdelorme.followUpYourGarden.core.services.exceptions;
 
+import fr.loicdelorme.followUpYourGarden.core.language.MyResourceBundle;
+
 /**
  * This exception is thrown if the wording is empty.
  * 
@@ -14,6 +16,6 @@ public class InvalidGroupOfPlantsWordingException extends Exception
 	 */
 	public InvalidGroupOfPlantsWordingException()
 	{
-		super("The wording is empty!");
+		super(MyResourceBundle.getBundle().getString("invalidGroupOfPlantsWordingException"));
 	}
 }
