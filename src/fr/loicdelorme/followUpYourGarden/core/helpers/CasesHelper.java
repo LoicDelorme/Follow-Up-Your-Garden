4b34f0ed8bb839d54224b7d0ffdfb17362249471
DELORME Loïc <loic.delorme.pro@gmail.com>
2015-06-24 18:24:41 +0200

    [update] helper.

--- src/fr/loicdelorme/followUpYourGarden/core/helpers/CasesHelper.java
+++ src/fr/loicdelorme/followUpYourGarden/core/helpers/CasesHelper.java
@@ -28,15 +28,15 @@ public class CasesHelper
 	 *            The height.
 	 * @param groupsOfPlants
 	 *            The list of groups of plants.
-	 * @param occupiedPositions
-	 *            The list of occupied positions.
 	 * @return A list of cases.
 	 */
-	public static List<Case> generateGlobalRepresentationCases(int width, int height, List<GroupOfPlants> groupsOfPlants, List<Position> occupiedPositions)
+	public static List<Case> generateGlobalRepresentationCases(int width, int height, List<GroupOfPlants> groupsOfPlants)
 	{
+		List<Position> occupiedPositions = new ArrayList<Position>();
 		Map<Integer, GroupOfPlants> groupsOfPlantsMap = new HashMap<Integer, GroupOfPlants>();
 		for (GroupOfPlants currentGroupOfPlants : groupsOfPlants)
 		{
+			occupiedPositions.addAll(currentGroupOfPlants.getPositions());
 			groupsOfPlantsMap.put(currentGroupOfPlants.getId(), currentGroupOfPlants);
 		}
 
@@ -46,13 +46,117 @@ public class CasesHelper
 		for (Position currentOccupiedPosition : occupiedPositions)
 		{
 			groupOfPlants = groupsOfPlantsMap.get(currentOccupiedPosition.getGroupOfPlantsId());
-			cases.add(new CaseWithGroupOfPlants(currentOccupiedPosition, groupOfPlants.getIconColor(), groupOfPlants));
+			if (groupOfPlants.getWording().equals("MAISON"))
+			{
+				cases.add(new Case(currentOccupiedPosition, Color.BROWN, false, true));
+			}
+			else
+			{
+				cases.add(new CaseWithGroupOfPlants(currentOccupiedPosition, groupOfPlants.getIconColor(), true, false, groupOfPlants));
+			}
 		}
 
 		List<Position> availablePositions = PositionsHelper.getAvailablePositions(width, height, occupiedPositions);
 		for (Position currentAvailablePosition : availablePositions)
 		{
-			cases.add(new Case(currentAvailablePosition, Color.WHITE));
+			cases.add(new Case(currentAvailablePosition, Color.WHITE, false, false));
+		}
+
+		cases.sort(null);
+
+		return cases;
+	}
+
+	/**
+	 * Generate cases for adding a group of plants.
+	 * 
+	 * @param width
+	 *            The width.
+	 * @param height
+	 *            The height.
+	 * @param groupsOfPlants
+	 *            The list of groups of plants.
+	 * @return A list of cases.
+	 */
+	public static List<Case> generateCasesForAddingGroupOfPlants(int width, int height, List<GroupOfPlants> groupsOfPlants)
+	{
+		List<Position> occupiedPositions = new ArrayList<Position>();
+		Map<Integer, GroupOfPlants> groupsOfPlantsMap = new HashMap<Integer, GroupOfPlants>();
+		for (GroupOfPlants currentGroupOfPlants : groupsOfPlants)
+		{
+			occupiedPositions.addAll(currentGroupOfPlants.getPositions());
+			groupsOfPlantsMap.put(currentGroupOfPlants.getId(), currentGroupOfPlants);
+		}
+
+		List<Case> cases = new ArrayList<Case>();
+
+		for (Position currentOccupiedPosition : occupiedPositions)
+		{
+			cases.add(new Case(currentOccupiedPosition, Color.BLACK, false, true));
+		}
+
+		List<Position> availablePositions = PositionsHelper.getAvailablePositions(width, height, occupiedPositions);
+		for (Position currentAvailablePosition : availablePositions)
+		{
+			cases.add(new Case(currentAvailablePosition, Color.WHITE, false, false));
+		}
+
+		cases.sort(null);
+
+		return cases;
+	}
+
+	/**
+	 * Generate cases for updating a group of plants.
+	 * 
+	 * @param width
+	 *            The width.
+	 * @param height
+	 *            The height.
+	 * @param groupsOfPlants
+	 *            The list of groups of plants.
+	 * @param groupOfPlantsToUdpdate
+	 *            The group of plants to update.
+	 * @return A list of cases.
+	 */
+	public static List<Case> generateCasesForUpdatingGroupOfPlants(int width, int height, List<GroupOfPlants> groupsOfPlants, GroupOfPlants groupOfPlantsToUdpdate)
+	{
+		List<Position> occupiedPositions = new ArrayList<Position>();
+		List<Position> occupiedPositionsByTheGroupOfPlantsToUdpdate = new ArrayList<Position>();
+		List<Position> allOccupiedPositions = new ArrayList<Position>();
+		Map<Integer, GroupOfPlants> groupsOfPlantsMap = new HashMap<Integer, GroupOfPlants>();
+		for (GroupOfPlants currentGroupOfPlants : groupsOfPlants)
+		{
+			if (currentGroupOfPlants.equals(groupOfPlantsToUdpdate))
+			{
+				occupiedPositionsByTheGroupOfPlantsToUdpdate.addAll(currentGroupOfPlants.getPositions());
+				allOccupiedPositions.addAll(currentGroupOfPlants.getPositions());
+				groupsOfPlantsMap.put(currentGroupOfPlants.getId(), currentGroupOfPlants);
+				continue;
+			}
+
+			occupiedPositions.addAll(currentGroupOfPlants.getPositions());
+			allOccupiedPositions.addAll(currentGroupOfPlants.getPositions());
+			groupsOfPlantsMap.put(currentGroupOfPlants.getId(), currentGroupOfPlants);
+		}
+
+		List<Case> cases = new ArrayList<Case>();
+
+		for (Position currentOccupiedPosition : occupiedPositions)
+		{
+			cases.add(new Case(currentOccupiedPosition, Color.BLACK, false, true));
+		}
+
+		GroupOfPlants groupOfPlants = groupsOfPlantsMap.get(occupiedPositionsByTheGroupOfPlantsToUdpdate.get(0).getGroupOfPlantsId());
+		for (Position currentOccupiedPositionByTheGroupOfPlantsToUdpdate : occupiedPositionsByTheGroupOfPlantsToUdpdate)
+		{
+			cases.add(new Case(currentOccupiedPositionByTheGroupOfPlantsToUdpdate, groupOfPlants.getIconColor(), true, false));
+		}
+
+		List<Position> availablePositions = PositionsHelper.getAvailablePositions(width, height, allOccupiedPositions);
+		for (Position currentAvailablePosition : availablePositions)
+		{
+			cases.add(new Case(currentAvailablePosition, Color.WHITE, false, false));
 		}
 
 		cases.sort(null);
