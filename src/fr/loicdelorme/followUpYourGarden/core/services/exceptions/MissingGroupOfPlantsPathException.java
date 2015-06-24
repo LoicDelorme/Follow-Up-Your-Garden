54e9a6fb28a3b494d06bb2461ff1c83411148b78
DELORME Loïc <loic.delorme.pro@gmail.com>
2015-06-24 11:50:36 +0200

    [update] exception.

--- src/fr/loicdelorme/followUpYourGarden/core/services/exceptions/MissingGroupOfPlantsPathException.java
+++ src/fr/loicdelorme/followUpYourGarden/core/services/exceptions/MissingGroupOfPlantsPathException.java
@@ -1,5 +1,7 @@
 package fr.loicdelorme.followUpYourGarden.core.services.exceptions;
 
+import fr.loicdelorme.followUpYourGarden.core.language.MyResourceBundle;
+
 /**
  * This exception is thrown if the path attribute is missing.
  * 
@@ -14,6 +16,6 @@ public class MissingGroupOfPlantsPathException extends Exception
 	 */
 	public MissingGroupOfPlantsPathException()
 	{
-		super("The path is missing!");
+		super(MyResourceBundle.getBundle().getString("missingGroupOfPlantsPathException"));
 	}
 }
