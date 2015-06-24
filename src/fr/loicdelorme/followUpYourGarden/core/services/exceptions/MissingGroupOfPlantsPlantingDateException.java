56218d534084b80d2fbebf293c190a97e530ed32
DELORME Loïc <loic.delorme.pro@gmail.com>
2015-06-24 11:50:33 +0200

    [update] exception.

--- src/fr/loicdelorme/followUpYourGarden/core/services/exceptions/MissingGroupOfPlantsPlantingDateException.java
+++ src/fr/loicdelorme/followUpYourGarden/core/services/exceptions/MissingGroupOfPlantsPlantingDateException.java
@@ -1,5 +1,7 @@
 package fr.loicdelorme.followUpYourGarden.core.services.exceptions;
 
+import fr.loicdelorme.followUpYourGarden.core.language.MyResourceBundle;
+
 /**
  * This exception is thrown if the plantingDate attribute is missing.
  * 
@@ -14,6 +16,6 @@ public class MissingGroupOfPlantsPlantingDateException extends Exception
 	 */
 	public MissingGroupOfPlantsPlantingDateException()
 	{
-		super("The planting date is missing!");
+		super(MyResourceBundle.getBundle().getString("missingGroupOfPlantsPlantingDateException"));
 	}
 }
