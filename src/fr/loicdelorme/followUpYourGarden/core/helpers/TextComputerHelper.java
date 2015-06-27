ebcf94c53a8dbc6adc4c83444b82048a0a80bae1
DELORME Loïc <loic.delorme.pro@gmail.com>
2015-06-27 16:34:24 +0200

    [update] helper.

--- src/fr/loicdelorme/followUpYourGarden/core/helpers/TextComputerHelper.java
+++ src/fr/loicdelorme/followUpYourGarden/core/helpers/TextComputerHelper.java
@@ -4,6 +4,7 @@ import java.util.List;
 
 import fr.loicdelorme.followUpYourGarden.core.language.MyResourceBundle;
 import fr.loicdelorme.followUpYourGarden.core.models.CarriedOutTask;
+import fr.loicdelorme.followUpYourGarden.core.models.TaskToBeCarryOut;
 
 /**
  * This class allow you to handle texts.
@@ -22,7 +23,7 @@ public class TextComputerHelper
 	 */
 	public static String computeCarriedOutTasksText(List<CarriedOutTask> carriedOutTasks)
 	{
-		if (carriedOutTasks.size() == 0)
+		if (carriedOutTasks.isEmpty())
 		{
 			return MyResourceBundle.getBundle().getString("groupOfPlantsSummaryComplementaryInformationNoHistoric");
 		}
@@ -34,6 +35,36 @@ public class TextComputerHelper
 			result.append(currentCarriedOutTask.getTypeOfTasks().getWording());
 			result.append(" : ");
 			result.append(currentCarriedOutTask.getCarriedOutDate());
+			result.append("\n");
+		}
+
+		return result.toString();
+	}
+
+	/**
+	 * Compute tasks to be carry out text.
+	 * 
+	 * @param tasksToBeCarryOut
+	 *            The list of tasks to be carry out.
+	 * @return The computed text.
+	 */
+	public static String computeTasksToBeCarryOutText(List<TaskToBeCarryOut> tasksToBeCarryOut)
+	{
+		if (tasksToBeCarryOut.isEmpty())
+		{
+			return MyResourceBundle.getBundle().getString("globalRepresentationComplementaryInformationNoTaskToBeCarryOut");
+		}
+
+		StringBuilder result = new StringBuilder();
+
+		for (TaskToBeCarryOut currentTaskToBeCarryOut : tasksToBeCarryOut)
+		{
+			result.append(currentTaskToBeCarryOut.getTypeOfTasks().getWording());
+			result.append(" : ");
+			result.append(currentTaskToBeCarryOut.getDeadlineDate());
+			result.append(" (");
+			result.append(currentTaskToBeCarryOut.getCurrentProgression());
+			result.append(" %)");
 			result.append("\n");
 		}
 
